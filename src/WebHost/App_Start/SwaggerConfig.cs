10ccbc76d3c32169efe793b2a032d475c0e56908
Jenya Y. <jenyay@gmail.com>
2016-01-07 23:25:37 +0200

    Check of XML file existance

--- src/WebHost/App_Start/SwaggerConfig.cs
+++ src/WebHost/App_Start/SwaggerConfig.cs
@@ -1,6 +1,7 @@
 using System.Web.Http;
 using Swashbuckle.Application;
 using System;
+using System.IO;
 
 namespace WebHost
 {
@@ -10,13 +11,15 @@ namespace WebHost
         public static void Register(HttpConfiguration httpConfigurations)
         {
             var thisAssembly = typeof(SwaggerConfig).Assembly;
+            var coreXml = new FileInfo($"{AppDomain.CurrentDomain.BaseDirectory}\\bin\\Core.XML");
+            var hostXml = new FileInfo($"{AppDomain.CurrentDomain.BaseDirectory}\\bin\\WebHost.XML");
 
             httpConfigurations
                 .EnableSwagger(c =>
                     {
                         c.SingleApiVersion("v1", "Enttoi API");
-                        c.IncludeXmlComments($"{AppDomain.CurrentDomain.BaseDirectory}\\bin\\Core.XML");
-                        c.IncludeXmlComments($"{AppDomain.CurrentDomain.BaseDirectory}\\bin\\WebHost.XML");
+                        if (coreXml.Exists) c.IncludeXmlComments(coreXml.FullName);
+                        if (hostXml.Exists) c.IncludeXmlComments(hostXml.FullName);
                     })
                 .EnableSwaggerUi();
         }
