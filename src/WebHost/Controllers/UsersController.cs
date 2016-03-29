8ec402b0ddb937cda99c2f8218b8b81e78c28b12
Jenya Y. <jenyay@gmail.com>
2016-03-30 00:00:23 +0300

    REST API support for online users; closes #1

---
+++ src/WebHost/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using Core.Services;
+using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebHost.Models;
+
+namespace WebHost.Controllers
+{
+    /// <summary>
+    /// Users API
+    /// </summary>
+    public class UsersController : ApiController
+    {
+        private readonly IDistributedCounter _counterService;
+
+        public UsersController(IDistributedCounter counterService)
+        {
+            if (counterService == null) throw new ArgumentNullException(nameof(counterService));
+            _counterService = counterService;
+        }
+
+        /// <summary>
+        /// Gets the count of user that currently connected to HUB.
+        /// </summary>
+        /// <returns>The number of online users</returns>
+        [Route("users/online")]
+        [ResponseType(typeof(OnlineUsers))]
+        public async Task<IHttpActionResult> GetSensors()
+        {
+            return Ok(new OnlineUsers { UsersCount = await _counterService.GetCurrent() });
+        }
+    }
+}
---
+++ src/WebHost/Models/OnlineUsers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebHost.Models
+{
+    public class OnlineUsers
+    {
+        /// <summary>
+        /// The amount of users.
+        /// </summary>
+        public int UsersCount { get; set; }
+    }
+}
